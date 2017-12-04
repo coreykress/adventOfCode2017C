0c29302de1907fd0d2fc1edd036a6b6ba3ec84ae
Corey Kress <corey.kress@coachmeplus.com>
2017-12-04 17:55:51 -0500

    finished day 2 part 1

--- cpp/day2/main.cpp
+++ cpp/day2/main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
 #include <string>
+#include <vector>
+#include <algorithm>
+
+std::vector<int> splitLine(std::string line, char delimiter) {
+    std::vector<int> results;
+    size_t pos = 0;
+    std::string value;
+
+    while ((pos = line.find(delimiter)) != std::string::npos) {
+        value = line.substr(0, pos);
+        results.push_back(std::stoi(value));
+        line.erase(0, pos + 1);
+    }
+
+    //get the last of the line
+    results.push_back(std::stoi(line));
+
+    return results;
+}
 
 int main (int argc, char *argv[])
 {
@@ -11,18 +29,25 @@ int main (int argc, char *argv[])
         return 1;
     }
 
+    std::string line;
     std::ifstream file(argv[1]);
+    int sum = 0;
+
     // check to see if the file opening succeeded
     if (!file.is_open()) {
         std::cout << "Ain't no file here boss" << std::endl;
         return 1;
     }
 
-    std::string line;
     while (std::getline(file, line)) {
-        std::istringstream buffer(line);
-        std::cout << &buffer << std::endl;
+        std::vector<int> currentRowValues = splitLine(line, '\t');
+        auto max = *std::max_element(currentRowValues.begin(), currentRowValues.end());
+        auto min = *std::min_element(currentRowValues.begin(), currentRowValues.end());
+
+        sum += (max - min);
     }
+
+    std::cout << sum << std::endl;
 
     return 0;
 }
